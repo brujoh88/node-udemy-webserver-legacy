c37d795261140e1b2feb8a81bea62318ef863785
Gus - MVC <gustavotiseira@gmail>
2024-02-22 13:07:25 -0300

    Clase 88 Req y Res)

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http')
 
 http.createServer((req,res)=>{
-    res.write('Hola mundo')
+    /* res.writeHead(200, {'Content-Type': 'application/json'})
+    const persona = {
+        id: 1,
+        nombre: "Gustavo"
+    }
+    res.write(JSON.stringify(persona)) */
+
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    res.writeHead(200, {'Content-Type': 'application/csv'})
+    
+    res.write('id,nombre\n')
+    res.write('1,Gustavo\n')
+    res.write('2,Fernando\n')
+    res.write('3,Hernan\n')
+
+
+
     res.end()
 }).listen(8080)
 
