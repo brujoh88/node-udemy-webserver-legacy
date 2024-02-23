49c803ed1c089f2757003f861dbea0381399c0cf
Gus - MVC <gustavotiseira@gmail>
2024-02-23 08:49:53 -0300

    Clase 90 Servir contenido estatico

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
+app.use( express.static('public'))
 
+//Ruta que ingresa por estatico y no por esta
+app.get('/ruta', (req, res) => {
+    res.send('aca nunca voy a entrar')
+  })
+
+//Ruta que ingresa por aca no existe en el estatico
 app.get('/hola-mundo', (req, res) => {
     res.send('Hello World!')
   })
 
-
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')
+    //res.send('404 | Page not found')
+    res.sendFile(__dirname + '/public/404.html')
+
   })
 
 app.listen(port, () => {
